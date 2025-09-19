7239c62b1d37461fb8e2a5c2817c5d5f142be49c
María Candelaria Fariña Rodríguez <mfarinardguez@gmail.com>
2025-09-19 13:03:27 +0100

    feat(index.js): Separate words user, coding and score

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const fs = require('fs');
 
 try {
     const data = fs.readFileSync('encoded-data.csv', 'utf8');
-    console.log(data);
-    
+
+    const lines = data.split('\n');
+    lines.map((line) => {
+        const score_user = line.split(',');
+    })
+
 } catch (err) {
    console.error(err);
 }
